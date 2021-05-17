ef331f10bd3559cda4630d8dcff4d1f4180f00f5
betterrahul5555 <63734203+betterrahul5555@users.noreply.github.com>
2021-05-17 22:20:11 +0200

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Nowe.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public override async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            ViewData["ip"] = context.HttpContext.Connection.RemoteIpAddress;
+           await next();
+        }
+
+    }
+}
